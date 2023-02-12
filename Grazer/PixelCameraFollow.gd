0088a558389f5a92f99c8a244bc50d98823acdfb
Wharfiggle <elijahsouthman@gmail.com>
2023-02-12 01:29:24 -0600

    Attempt to Make Camera Move Only with Pixels
    Instead of a freely moving 3D camera I wanted to restrict the camera to moving along with the pixels of our viewport to make the fake 2D look more convincing.

---
+++ Grazer/PixelCameraFollow.gd
@@ -0,0 +1,28 @@
+extends Camera
+
+# Declare member variables here.
+export (int) var screenWidth = 640
+export (int) var screenHeight = 360
+export (float) var lerpSpeed = 3
+export (NodePath) var targetNodePath
+var followTarget
+var camOffset
+var pos
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	followTarget = get_node(targetNodePath)
+	camOffset = self.translation
+	pos = self.translation
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pos = lerp(pos, followTarget.global_translation, lerpSpeed * delta)
+	self.translation = pos + camOffset
+	self.translation -= Vector3(
+	#42 units accross the screen horizontally with camera size 30
+		fmod(self.translation.x * screenWidth / 42.0, 1) * 42.0 / screenWidth,
+		fmod(self.translation.y * screenWidth / 42.0, 1) * 42.0 / screenWidth,
+		fmod(self.translation.z * screenWidth / 42.0, 1) * 42.0 / screenWidth)
+	print(self.translation)
+	#self.translation = followTarget.global_translation + camOffset
