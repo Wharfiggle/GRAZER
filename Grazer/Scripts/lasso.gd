5735c243649e15162088bed48d25092aceb7a54d
mellandm1350 <mellandm1350@my.uwstout.edu>
2023-02-26 16:54:02 -0600

    Basic lasso
    Should try and find the target (cow) and have a chance to miss

---
+++ Grazer/Scripts/lasso.gd
@@ -0,0 +1,34 @@
+extends KinematicBody
+
+var lasso: MeshInstance
+var target
+var lassoSpeed: float = 5.0
+var lassoRange: float = 5.0
+var lassoHead: Vector3
+
+func _ready():
+	#lasso = $MeshInstance
+	lasso.visible = false
+
+func _process(delta: float):
+	if target and target.is_visible_in_tree():
+		lasso.visible = true
+		var distance = (target.global_transform.origin - global_transform.origin).length()
+		if distance <= lassoRange:
+			lassoHead = (target.global_transform.origin - global_transform.origin).normalized()
+			move_and_slide(lassoHead * lassoSpeed)
+		else:
+			target = null
+			lasso.visible = false
+
+func throw_lasso(target, missRange: float = 2.0):
+	var distance = (target.global_transform.origin - global_transform.origin).length()
+	if distance <= lassoRange:
+		var missDistance = randf() * missRange
+		if randf() > 0.5:
+			missDistance *= -1
+		var missOffset = lassoHead * missDistance
+		self.target = target.global_transform.origin + missOffset
+	else:
+		self.target = null
+		lasso.visible = false
