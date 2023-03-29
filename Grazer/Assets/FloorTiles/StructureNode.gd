4a074089a37c3116ddd0ca6983ad017e9c97be02
Andersoni0723 <andersoni0723@my.uwstout.edu>
2023-03-29 17:25:41 -0500

    Start of structure nodes

---
+++ Grazer/Assets/FloorTiles/StructureNode.gd
@@ -0,0 +1,15 @@
+extends Node3D
+
+var pathname = ""
+var width = 1
+var depth = 1
+var renderRange = 3
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
