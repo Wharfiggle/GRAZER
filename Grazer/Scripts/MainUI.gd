9f1cfbc1fdb637985e3b59a480c0adc9b5ee2364
SeerOfTheGates <100220504+SeerOfTheGates@users.noreply.github.com>
2023-04-10 19:47:17 -0500

    Update to health bar code (Bounce)

--- Grazer/Scripts/MainUI.gd
+++ Grazer/Scripts/MainUI.gd
@@ -2,16 +2,30 @@ extends MarginContainer
 
 @onready var healthBar = $HBoxContainer/Bars/bar/TextureProgressBar
 
-@onready var player = get_node("/root/Level/Player")
+@onready var tween = get_tree().create_tween()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	healthBar.max_value=player.maxHitpoints
 	
 	
+	pass
 	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
+	
 	pass
+
+
+
+func _on_health_update_(health, amount):
+	healthBar.value=health
+	
+	tween.tween_property(healthBar, "value", healthBar.value, 0.5).set_trans(Tween.TRANS_BOUNCE)
+	
+
+
+func _on_max_health_update_(maxHealth):
+	healthBar.max_value =maxHealth
