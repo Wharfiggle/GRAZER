99502d38d017898df1008d64733e552429075dd2
SeerOfTheGates <100220504+SeerOfTheGates@users.noreply.github.com>
2023-04-14 12:14:16 -0500

    health bar update (damage shown) begining of ammo UI

--- Grazer/Scripts/MainUI.gd
+++ Grazer/Scripts/MainUI.gd
@@ -1,31 +1,12 @@
-extends MarginContainer
+extends Control
 
-@onready var healthBar = $HBoxContainer/Bars/bar/Node2D/TextureProgressBar
-
-@onready var tween = get_tree().create_tween()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
-	
-	pass
-	
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
 	
-	
+func _process(delta):
 	pass
-
-
-
-func _on_health_update_(health):
-	healthBar.value = health * 10
-	
-	#tween.tween_property(healthBar, "value", healthBar.value, 0.5).set_trans(Tween.TRANS_BOUNCE)
-	
-
-
-func _on_max_health_update_(maxHealth):
-	healthBar.max_value =maxHealth
