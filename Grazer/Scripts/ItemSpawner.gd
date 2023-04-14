2d15352478325916e18acfcce853693db04c154c
Wharfiggle <elijahsouthman@gmail.com>
2023-04-14 12:17:00 -0500

    Fixed Item Spawner

--- Grazer/Scripts/ItemSpawner.gd
+++ Grazer/Scripts/ItemSpawner.gd
@@ -10,12 +10,16 @@ var rng = RandomNumberGenerator.new()
 
 func _ready():
 	get_child(0).queue_free()
+	rng.randomize()
 
-func spawn(inChanceMod:float, _inPrefabs:Array):
+func spawn(inChanceMod:float, inPrefabs:Array):
+	print("spawn called on item spawner: " + str(spawnChance) + " : " + str(inChanceMod))
 	var rn = rng.randf()
 	if(rn <= spawnChance * inChanceMod):
 		var instance = itemDrop.instantiate()
 		instance.randomItem = randomItem
 		instance.potionVsUpgradeChance = potionVsUpgradeChance
 		instance.itemID = itemID
+		instance.global_position = global_position
+		get_node(NodePath("/root/Level")).add_child(instance)
 	queue_free()
