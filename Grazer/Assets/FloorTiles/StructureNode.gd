431a69a72c38c1c6d237acf1d3391356a5d7dc2f
wdsagstetter <100251863+wdsagstetter@users.noreply.github.com>
2023-04-18 19:47:27 -0500

    rv and some other bits added!

--- Grazer/Assets/FloorTiles/StructureNode.gd
+++ Grazer/Assets/FloorTiles/StructureNode.gd
@@ -31,6 +31,7 @@ static func retrieveStructureTypes() -> Array:
 	structures.append(["res://Assets/FloorTiles/TilePool/StructureTiles/EmptyFloor1.tscn", 2, 2]) #test
 	structures.append(["res://Assets/FloorTiles/TilePool/StructureTiles/testTile.tscn", 2, 2]) #test
 	structures.append(["res://Assets/FloorTiles/TilePool/StructureTiles/cliffPit1.tscn", 1, 1]) #test
+	structures.append(["res://Assets/FloorTiles/TilePool/StructureTiles/rvTestStruct.tscn", 2, 1])
 	
 	# * ^ ADD NEW STRUCTURES HERE ^ *
 	#sPathname is the path to the scene. Right click and click "Copy Path" in the explorer to get it
