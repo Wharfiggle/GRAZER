c59703bd341bf7630064f6f0aae0eb87f7814f09
Howie-Ramaley <ramaleyh8995@my.uwstout.edu>
2023-04-25 16:14:28 -0500

    Credits Screen

--- Grazer/Prefabs/MainMenuScript.gd
+++ Grazer/Prefabs/MainMenuScript.gd
@@ -16,8 +16,11 @@ func _on_button_pressed():
 	get_tree().change_scene_to_file("res://Levels/Level.tscn")
 	
 
-
 func _on_quit_button_pressed():
 	
 	get_tree().quit()
 	pass # Replace with function body.
+
+
+func _on_credits_pressed():
+	get_tree().change_scene_to_file("res://Prefabs/credits.tscn")
