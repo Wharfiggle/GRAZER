61c57be7017946c854b6a17afa04a36ad3ad0adf
Wharfiggle <elijahsouthman@gmail.com>
2023-04-25 20:16:04 -0500

    Better Main Menu + Credits

--- Grazer/Scripts/credits.gd
+++ Grazer/Scripts/credits.gd
@@ -1,14 +1,10 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if(Input.is_action_just_pressed("dodge")):
+		_on_button_pressed()
 
 
 func _on_button_pressed():
