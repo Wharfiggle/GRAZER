222b0400c2a811db0dcb844d56c7a7ae59b3b822
Wharfiggle <elijahsouthman@gmail.com>
2023-08-28 20:59:08 -0500

    Nerfed Silo

--- Grazer/Assets/FloorTiles/StructureNode.gd
+++ Grazer/Assets/FloorTiles/StructureNode.gd
@@ -31,12 +31,10 @@ static func retrieveStructureTypes() -> Array:
 	structures.append(["res://Assets/FloorTiles/TilePool/StructureTiles/checkPointLeftV2.tscn", 4, 4]) #checkpoint left
 	structures.append(["res://Assets/FloorTiles/TilePool/StructureTiles/checkPointRightV2.tscn", 4, 4]) #checkpoint right
 	structures.append(["res://Assets/FloorTiles/TilePool/StructureTiles/startArea.tscn", 2, 3]) #start area
-	structures.append(["res://Assets/FloorTiles/TilePool/StructureTiles/finalFieldNoGrass.tscn", 2, 2])
-#	structures.append(["res://Assets/FloorTiles/TilePool/StructureTiles/structure2.tscn", 2, 1])
-#	structures.append(["res://Assets/FloorTiles/TilePool/StructureTiles/EmptyFloor1.tscn", 2, 2]) #test
-	structures.append(["res://Assets/FloorTiles/TilePool/StructureTiles/testTile.tscn", 2, 2]) #test
-	structures.append(["res://Assets/FloorTiles/TilePool/StructureTiles/cliffPit1.tscn", 1, 2]) #test
-	#structures.append(["res://Assets/FloorTiles/TilePool/StructureTiles/cliffPitV2.tscn", 1, 1]) #test
+	structures.append(["res://Assets/FloorTiles/TilePool/StructureTiles/finalFieldNoGrass.tscn", 2, 2]) #end area
+	
+	structures.append(["res://Assets/FloorTiles/TilePool/StructureTiles/testTile.tscn", 2, 2])
+	structures.append(["res://Assets/FloorTiles/TilePool/StructureTiles/cliffPit1.tscn", 1, 2])
 	structures.append(["res://Assets/FloorTiles/TilePool/StructureTiles/rvTestStruct.tscn", 2, 1])
 	structures.append(["res://Assets/FloorTiles/TilePool/StructureTiles/archway.tscn", 1, 1])
 	structures.append(["res://Assets/FloorTiles/TilePool/StructureTiles/radioTower.tscn", 1, 1])
@@ -45,6 +43,10 @@ static func retrieveStructureTypes() -> Array:
 	structures.append(["res://Assets/FloorTiles/TilePool/StructureTiles/organCactus1.tscn", 1, 1])
 	structures.append(["res://Assets/FloorTiles/TilePool/StructureTiles/ruins.tscn", 3, 3])
 	structures.append(["res://Assets/FloorTiles/TilePool/StructureTiles/cliffPitV2.tscn", 1, 1])
+
+	#structures.append(["res://Assets/FloorTiles/TilePool/StructureTiles/structure2.tscn", 2, 1]) #test
+	#structures.append(["res://Assets/FloorTiles/TilePool/StructureTiles/EmptyFloor1.tscn", 2, 2]) #test
+	#structures.append(["res://Assets/FloorTiles/TilePool/StructureTiles/cliffPitV2.tscn", 1, 1]) #test
 	
 	# * ^ ADD NEW STRUCTURES HERE ^ *
 	#sPathname is the path to the scene. Right click and click "Copy Path" in the explorer to get it
